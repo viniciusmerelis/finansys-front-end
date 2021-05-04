9a23be84ebf70deed53aa7eb116016a38e524594
Vinícus Merelis <vinicius.merelis@gmail.com>
2021-05-04 08:50:06 -0300

    Finalizado Entry component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
-
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MomentDateModule } from '@angular/material-moment-adapter';
+import { MatNativeDateModule, MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { IMaskModule } from 'angular-imask';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { EntryFormComponent } from './components/entry-form/entry-form.component';
 import { EntryListComponent } from './components/entry-list/entry-list.component';
 import { EntriesRoutingModule } from './entries-routing.module';
-import { IMaskModule } from 'angular-imask';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
 
 
 
@@ -16,7 +17,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     EntriesRoutingModule,
     IMaskModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MomentDateModule
   ]
 })
 export class EntriesModule { }
